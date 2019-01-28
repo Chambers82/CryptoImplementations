6be6808bb246551c4b52b7cd5baf65572c0c435b
Brent E. Chambers <Chambers82@users.noreply.github.com>
2019-01-28 02:29:43 -0500

    small errors

--- aesencrypt.cpp
+++ aesencrypt.cpp
@@ -579,13 +579,6 @@ Exit_MyEncryptFile:
 } // End Encryptfile.
 
 
-//-------------------------------------------------------------------
-//  This example uses the function MyHandleError, a simple error
-//  handling function, to print an error message to the  
-//  standard error (stderr) file and exit the program. 
-//  For most applications, replace this function with one 
-//  that does more extensive error reporting.
-
 void MyHandleError(LPTSTR psz, int nErrorNumber)
 {
     _ftprintf(stderr, TEXT("An error occurred in the program. \n"));
